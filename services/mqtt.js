6b64d5e192207f80891a4b9168894755693c9165
Luis Miguel <luismiguel010@gmail.com>
2020-03-26 20:36:16 -0500

    Me vomito encima de JavaScript

--- services/mqtt.js
+++ services/mqtt.js
@@ -1,18 +1,39 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://192.168.1.3')
+var environment = require('../controllers/environment')
 
-client.on('connect', function () {
-    client.subscribe('prueba/mqtt/javascript', function (err) {
-        if (!err) {
-            client.publish('prueba/mqtt/javascript', 'Hello mqtt')
-        }
+var growbed_client = mqtt.connect('mqtt://192.168.1.3')
+var greenroom_client = mqtt.connect('mqtt://192.168.1.3')
+var access_client = mqtt.connect('mqtt://192.168.1.3')
+
+growbed_client.on('connect', function growbed_message() {
+    growbed_client.subscribe('medicion/zona/x/invernadero/y/cama/z/ambiente', function (err) {
+        console.log("Subscrito al tópico correctamente medicion/zona/x/invernadero/y/cama/z/ambiente.")
     })
 })
-
-client.on('message', function (topic, message) {
-    console.log(message.toString())
-    client.end
+growbed_client.on('message', function (topic, message) {
+    console.log(message.toString());
+    environment.save_grow_bed_environment_registre(message.toString());
+    client_growbed.end
 })
 
+greenroom_client.on('connect', function () {
+    greenroom_client.subscribe('medicion/zona/x/invernadero/y/ambiente', function (err) {
+        console.log("Subscrito al tópico correctamente medicion/zona/x/invernadero/y/ambiente.")
+    })
+})
+greenroom_client.on('message', function (topic, message) {
+    console.log(message.toString())
+    greenroom.end
+})
+
+access_client.on('connect', function () {
+    access_client.subscribe('medicion/zona/x/invernadero/y/ingresos', function (err) {
+        console.log("Subscrito al tópico correctamente medicion/zona/x/invernadero/y/ingresos.")
+    })
+})
+access_client.on('message', function (topic, message) {
+    console.log(message.toString())
+    greenroom.end
+})
 
 
