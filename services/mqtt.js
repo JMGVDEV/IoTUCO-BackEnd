7524342aa87d4e88bad6da65c8460fd359bb1597
Luis Miguel <luismiguel010@gmail.com>
2020-04-03 09:36:48 -0500

    Add function for publish

--- services/mqtt.js
+++ services/mqtt.js
@@ -5,6 +5,7 @@ var environment = require("../controllers/environment");
 var growbed_client = mqtt.connect(config.MQTT_CONF.host, config.MQTT_CONF);
 var greenroom_client = mqtt.connect(config.MQTT_CONF.host, config.MQTT_CONF);
 var access_client = mqtt.connect(config.MQTT_CONF.host, config.MQTT_CONF);
+var control_client = mqtt.connect(config.MQTT_CONF.host, config.MQTT_CONF);
 
 // -------------------------------------------------------------------------
 growbed_client.on("connect", () => {
@@ -58,3 +59,11 @@ access_client.on("message", function(topic, message) {
   //console.log(message.toString());
   access_client.end;
 });
+
+function publish_growbed(peripheral, value, zone, greenroom, growbed){
+    control_client.publish('medicion/zona/${zone}/invernadero/${greenroom}/cama/${growbed}/ambiente','{peripheral:${peripheral},value:${value}}')
+}
+
+function publish_greenroom(peripheral, value, zone, greenroom){
+    control_client.publish('medicion/zona/${zone}/invernadero/${greenroom}','{peripheral:${peripheral},value:${value}}')
+}
