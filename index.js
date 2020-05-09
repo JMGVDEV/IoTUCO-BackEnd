a691d4ad23428ca6ae6e3150ca397f6df834dd63
Luis Miguel <luismiguel010@gmail.com>
2020-05-09 12:05:46 -0500

    server static

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require("./config/config");
 const models = require("./controllers/index");
 const path = require('path');
+const routes = ['/home','/users','/diseases','/configactions','/'];
 let bodyParser = require("body-parser");
 let express = require("express");
 let app = express();
@@ -54,7 +55,7 @@ models.sync_models();
  *              Config web page
  *-------------------------------------------------------*/
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', function(req, res) {
+app.get(routes, function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
